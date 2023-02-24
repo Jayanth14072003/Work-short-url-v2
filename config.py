05bb2ce0c8093b38d9a543ac3fb93ece09cf24cc
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-02-25 00:17:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,10 @@ ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+
+ADMINS.append(Id Owned Id)
+
 
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0)) 
